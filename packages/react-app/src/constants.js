2aa59fa908cf3e3cc1d3017e1c7adf4d727a08ae
Adejola <54010799+Adej0la@users.noreply.github.com>
2022-04-02 07:50:59 +0100

    update constant.js
    updated infura and etherscan API keys

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,8 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "8996ae3c08254f178506a939f8d4ba72";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "8EQ1T6MZR3T6Y3QJQTHINQG7FHYA9QJK28";
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
